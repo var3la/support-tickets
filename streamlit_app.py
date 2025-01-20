7a8ff3689774c4744956998576330c92bd12c9af
var3la <94931950+var3la@users.noreply.github.com>
2025-01-20 10:59:13 +0000

    traducciones

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,10 @@ import streamlit as st
 
 # Show app title and description.
 st.set_page_config(page_title="Support tickets", page_icon="🎫")
-st.title("🎫 Support tickets")
+st.title("🎫 Gestión tickets")
 st.write(
     """
-    This app shows how you can build an internal tool in Streamlit. Here, we are 
-    implementing a support ticket workflow. The user can create a ticket, edit 
-    existing tickets, and view some statistics.
+App para gestionar tus tareas
     """
 )
 
